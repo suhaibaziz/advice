74ad4cd5dab2145d9a584c7144553cfea994e639
suhaibaziz <[sohaibab21@gmail.com]>
2023-12-25 10:47:31 -0800

    Your message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import axios from 'axios';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter, faFacebook } from '@fortawesome/free-brands-svg-icons';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    advice: '',
+  };
+
+  componentDidMount() {
+    this.fetchAdvice();
+  }
+
+  fetchAdvice = () => {
+    axios.get('https://api.adviceslip.com/advice')
+      .then((response) => {
+        const { advice } = response.data.slip;
+        this.setState({ advice });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  handleTwitterClick = () => {
+    window.open(`https://twitter.com/intent/tweet?text=${encodeURIComponent(this.state.advice)}`, '_blank');
+  }
+
+  handleFacebookClick = () => {
+    window.open(`https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(window.location.href)}`, '_blank');
+  }
+
+  render() {
+    const { advice } = this.state;
+
+    return (
+      <div className='app'>
+        <div className='card'>
+          <h1 className='heading'>{advice}</h1>
+          <button className='button' onClick={this.fetchAdvice}>
+            <span>Give me advice!</span>
+          </button>
+
+          {/* Icon Row */}
+          <div className='icon-row'>
+            {/* Twitter Icon */}
+            <div className='icon' onClick={this.handleTwitterClick}>
+              <FontAwesomeIcon icon={faTwitter} style={{ color: '#1DA1F2' }} />
+            </div>
+
+            {/* Gap */}
+            <div className='icon-gap'></div>
+
+            {/* Facebook Icon */}
+            <div className='icon' onClick={this.handleFacebookClick}>
+              <FontAwesomeIcon icon={faFacebook} style={{ color: '#1877F2' }} />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
